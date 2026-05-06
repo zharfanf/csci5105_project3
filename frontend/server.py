8786294903007e1bafe60519880902f8352abbef
zharfanf <zharfanf@gmail.com>
2026-05-05 19:29:14 -0500

    refactor interfaces and support quorum-based strategy

--- frontend/server.py
+++ frontend/server.py
@@ -15,7 +15,10 @@ STORAGE_PORT = os.environ.get("STORAGE_PORT", "50052")
 STORAGE_TARGETS = [
     f"storage-0.storage:{STORAGE_PORT}",
     f"storage-1.storage:{STORAGE_PORT}",
+    f"storage-2.storage:{STORAGE_PORT}",
 ]
+STORAGE_COUNT = 3
+ITEM_ID = 0
 
 
 # def storage_target(key: str) -> str:
@@ -30,7 +33,7 @@ def storage_target(key: str) -> str:
 # helper functions for converting between dicts and protobuf messages
 def to_proto(d):
     return marketplace_pb2.Item(
-        item_id=d.get("item_id", ""),
+        item_id=d.get("item_id", ITEM_ID),
         seller_id=d.get("seller_id", ""),
         title=d.get("title", ""),
         category=d.get("category", ""),
@@ -61,150 +64,127 @@ class Frontend(marketplace_pb2_grpc.FrontendServiceServicer):
     # ASSUME THERE IS ONLY 1 MACHINE AT THE MOMENT
     def __init__(self):
         # Maybe hashing the item_id wouldn't be a bad idea
-        self.ITEM_ID = 0
+        # self.ITEM_ID = 0
+        pass
+    
+    def submit_task(self, task, **kwargs):
+        with futures.ThreadPoolExecutor(workers=3) as executor:
+            # Submit tasks with different completion times
+            futures = [executor.submit(task, target, **kwargs) for target in STORAGE_TARGETS]
+            
+            print("Waiting up to 2 seconds for tasks to finish...")
+            
+            # wait() pauses the main program for up to 3 seconds, then moves on
+            done, not_done = futures.wait(futures, timeout=2.0)
+            
+            # Process the ones that finished
+            print(f"\n--- Finished Tasks ({len(done)}) ---")
+            for future in done:
+                print(future.result())
+                
+            # Acknowledge the ones that didn't, and let them be
+            print(f"\n--- Tasks left behind ({len(not_done)}) ---")
+            print("These are still running in the background, but we aren't waiting.")
 
-    def CreateItem(self, request, context):
-        # TODO:
-        # Assumption: no duplicate items exists
-        
-        ## Incoming request
-        # message CreateItemRequest {
-        #     string seller_id = 1;
-        #     string title = 2;
-        #     string category = 3;
-        #     string description = 4;
-        #     double starting_price = 5;
-        #     int32 quantity = 6;
-        #     string status = 7;
-        # }
+            return done, not_done
 
-        ## Turn into        
-        # message StorageWriteRequest {
-        #     Item item = 1;
-        #     string operation = 2; // create, update, bid
-        #     int64 proposed_version = 3;
-        # }
-        
-        ## Conversion the request to target request
-        requestItem = to_proto(request)
-        target = storage_target("write")
+    
+    def CreateItem_helper(self, target, item):
         with grpc.insecure_channel(target) as channel:
             stub = marketplace_pb2_grpc.StorageServiceStub(channel)
             response = stub.Write(marketplace_pb2.StorageWriteRequest(
-                item=requestItem, operation="create", proposed_version=1))
-        
-        # item_id = self.ITEM_ID
-        if(response.success):
-            self.ITEM_ID += 1
-        
-        print(f"{POD_NAME} CREATED ITEM -> {target}", flush=True)
-        return marketplace_pb2.CreateItemResponse(
-            success=response.success, 
-            item_id=response.item.item_id, 
-            message=response.message)
+                item=item, operation="create", proposed_version=1))
+            
+            return response
 
-    def GetItem(self, request, context):
-        target = storage_target("read")
+    def CreateItem(self, request, context):
+        requestItem = to_proto(request)
+
+        response_done, _ = self.submit_task(self.CreateItem_helper, item=requestItem)
+        if(response_done[0].success):
+            ITEM_ID += 1
+
+        return marketplace_pb2.CreateItemResponse(
+            success=response_done[0].success, 
+            item_id=response_done[0].item.item_id, 
+            message=response_done[0].message)
+
+
+    def GetItem_helper(self, target, item_id):
         with grpc.insecure_channel(target) as channel:
             stub = marketplace_pb2_grpc.StorageServiceStub(channel)
-            response = stub.Read(marketplace_pb2.StorageReadRequest(item_id=request.item_id))
-            
-        print(f"{POD_NAME} GET ITEM -> {target}", flush=True)
-        return marketplace_pb2.GetItemResponse(
-            success=response.success,
-            item=response.item,
-            message=response.message)
+            response = stub.Read(marketplace_pb2.StorageReadRequest(item_id=item_id))
 
-    def SearchItems(self, request, context):
-        # message SearchItemsRequest {
-        #     string keyword = 1;
-        #     string category = 2;
-        # }
-
-        ## Turn Into
+            return response
         
-        target = storage_target("read")
+    def GetItem(self, request, context):
+        response_done, _ = self.submit_task(self.GetItem_helper, item=request.item_id)
+
+        # check if the results are consistent, if not then repair, pass this for now
+        
+        return marketplace_pb2.GetItemResponse(
+            success=response_done[0].success,
+            item=response_done[0].item,
+            message=response_done[0].message)
+    
+    
+    def SearchItems_helper(self, target, keyword, category):
         with grpc.insecure_channel(target) as channel:
             stub = marketplace_pb2_grpc.StorageServiceStub(channel)
             response = stub.Search(marketplace_pb2.StorageSearchRequest(
-                keyword=request.keyword,
-                category=request.category
+                keyword=keyword,
+                category=category
             ))
+
+            return response
+        
+    def SearchItems(self, request, context):
+        response_done, _ = self.submit_task(self.SearchItems_helper, keyword=request.keyword, category=request.category)
+
         print(f"{POD_NAME} SEARCHED -> {target}", flush=True)
         return marketplace_pb2.SearchItemsResponse(
-            success=response.success, 
-            items=response.items)
+            success=response_done[0].success, 
+            items=response_done[0].items)
     
+    
+    def UpdateItem_helper(self, target, item):
+        with grpc.insecure_channel(target) as channel:
+            stub = marketplace_pb2_grpc.StorageServiceStub(channel)
+            response = stub.Write(marketplace_pb2.StorageWriteRequest(
+                item=item, operation="update", proposed_version=1))
+            
+            return response
+        
     def UpdateItem(self, request, context):
-        # TODO:
-        # Assumption: no duplicate items exists
-        
-        ## Incoming request
-        # message CreateItemRequest {
-        #     string seller_id = 1;
-        #     string title = 2;
-        #     string category = 3;
-        #     string description = 4;
-        #     double starting_price = 5;
-        #     int32 quantity = 6;
-        #     string status = 7;
-        # }
-
-        ## Turn into        
-        # message StorageWriteRequest {
-        #     Item item = 1;
-        #     string operation = 2; // create, update, bid
-        #     int64 proposed_version = 3;
-        # }
-        
-        ## Conversion the request to target request
         requestItem = to_proto(request)
 
-        target = storage_target("write")
-        with grpc.insecure_channel(target) as channel:
-            stub = marketplace_pb2_grpc.StorageServiceStub(channel)
-            response = stub.Write(marketplace_pb2.StorageWriteRequest(
-                item=requestItem, operation="update", proposed_version=1))
+        response_done, _ = self.submit_task(self.UpdateItem_helper, item=requestItem)
         
-        # item_id = self.ITEM_ID
-        
-        print(f"{POD_NAME} UPDATED ITEM -> {target}", flush=True)
+        print(f"{POD_NAME} UPDATED ITEM -> {STORAGE_TARGETS}", flush=True)
         return marketplace_pb2.UpdateItemResponse(
-            success=response.success, 
-            item=response.item, 
-            message=response.message)
+            success=response_done[0].success, 
+            item=response_done[0].item, 
+            message=response_done[0].message)
+    
+    
+    def PlaceBid_helper(self, target, item):
+        with grpc.insecure_channel(target) as channel:
+            stub = marketplace_pb2_grpc.StorageServiceStub(channel)
+            response = stub.Write(marketplace_pb2.StorageWriteRequest(
+                item=item, operation="bid", proposed_version=1))
+            
+            return response
     
     def PlaceBid(self, request, context):
         requestItem = to_proto(request)
 
-        target = storage_target("write")
-        with grpc.insecure_channel(target) as channel:
-            stub = marketplace_pb2_grpc.StorageServiceStub(channel)
-            response = stub.Write(marketplace_pb2.StorageWriteRequest(
-                item=requestItem, operation="bid", proposed_version=1))
+        response_done, _ = self.submit_task(self.PlaceBid_helper, item=requestItem)
         
-        
-        print(f"{POD_NAME} PLACED A BID -> {target}", flush=True)
+        print(f"{POD_NAME} PLACED A BID -> {STORAGE_TARGETS}", flush=True)
         return marketplace_pb2.PlaceBidResponse(
-            success=response.success, 
-            current_price=response.current_price, 
-            message=response.message)
-    
-    def PlaceBid(self, request, context):
-        requestItem = to_proto(request)
-
-        target = storage_target("write")
-        with grpc.insecure_channel(target) as channel:
-            stub = marketplace_pb2_grpc.StorageServiceStub(channel)
-            response = stub.Write(marketplace_pb2.StorageWriteRequest(
-                item=requestItem, operation="bid", proposed_version=1))
-        
-        
-        print(f"{POD_NAME} PLACED A BID -> {target}", flush=True)
-        return marketplace_pb2.PlaceBidResponse(
-            success=response.success, 
-            current_price=response.current_price, 
-            message=response.message)
+            success=response_done[0].success, 
+            current_price=response_done[0].current_price, 
+            message=response_done[0].message)
     
     # def JoinAuction(self, request, context):
     #     requestItem = to_proto(request)
